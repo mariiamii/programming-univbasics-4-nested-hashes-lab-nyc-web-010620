735e248b8119c35813d37b6b00598b075110299f
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-19 21:56:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ programmer_hash =
           :languages => ["C"]
         }
      }	
-	programmer_hash[:alan_kay][:known_for]
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages] 
 end
 
 def adding_matz
