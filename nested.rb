a5c2ee7e797da3904efcd397aa3acbe89189c89b
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-19 22:18:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,22 +37,21 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-        programmer_hash[:dennis_ritchie][:languages][0]
+	programmer_hash = {
+    :grace_hopper => {
+      :known_for => "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
+    },
+    :alan_kay => {
+      :known_for => "Object Orientation",
+      :languages => ["Smalltalk", "LISP"]
+    },
+    :dennis_ritchie => {
+      :known_for => "Unix",
+      :languages => ["C"]
+    }
+  }
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
